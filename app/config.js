4f2a76d783074e7950202a55cbb06de6bcf2fd4d
Cosmin Iordache <cosmin2iordache@gmail.com>
2022-02-10 13:48:51 +0000

    2 steps order

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'COVID19 - LFT Distribution System',
 
   // Default port that prototype runs on
   port: '3000',
